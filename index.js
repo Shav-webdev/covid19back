43e030bd2d57db66bf5c324698ba48b811f91513
Shavarsh <shavarsh.abrahamyan1993@gmail.com>
2020-03-29 11:39:54 +0400

    feat:installed cors package

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const userRouter = require("./api/routes/user.routes");
 const { db } = require("./db/db");
+const cors = require("cors");
+
 
 const app = express();
+app.use(morgan("combined"));
+app.use(cors);
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/src"));
 app.use(express.static(__dirname + "/server/api"));
-app.use(morgan("combined"));
 app.use(userRouter);
 
 const PORT = process.env.PORT || 5000;
